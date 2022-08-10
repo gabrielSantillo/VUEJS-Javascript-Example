50644ba0ebabba70adec54013d93830ed8f84761
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-09 19:14:02 -0600

    Finished the knowledge check

---
+++ src/components/ScriptExample.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                tweets: [`01_tweet`, `02_tweet`, `03_tweet`],
+                num_friends : 10,
+                logged_in: true
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
