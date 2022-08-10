fa04381b1f23d9137638b99a3331b13083d59b83
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-09 20:12:17 -0600

    Finished the knowledge check

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-
+    <script-example></script-example>
   </div>
 </template>
 
 <script>
-
+import ScriptExample from '@/components/ScriptExample.vue'
 
 export default {
   name: 'App',
   components: {
-
+    ScriptExample
   }
 }
 </script>
--- src/components/ScriptExample.vue
+++ src/components/ScriptExample.vue
@@ -1,11 +1,26 @@
 <template>
     <div>
+        <p>{{ num_friends }}</p>
+        <p>{{ logged_in }}</p>
+        <p v-for="tweet in tweets" :key="tweet">
+             {{ tweet }}
+        </p>
 
     </div>
 </template>
 
 <script>
     export default {
+        mounted () {
+            this.show_info();
+        },
+
+        methods: {
+            show_info() {
+                
+            }
+        },
+
         data() {
             return {
                 tweets: [`01_tweet`, `02_tweet`, `03_tweet`],
